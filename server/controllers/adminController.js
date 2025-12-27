bad30bb102baa405907fbcfe07209009cd621093
Prinston Dsouza <prinstondsouza42@gmail.com>
2025-12-27 22:40:23 +0530

    new commit

--- server/controllers/adminController.js
+++ server/controllers/adminController.js
@@ -1,8 +1,17 @@
-
+// Admin dashboard welcome route
+export const adminDashboard = (req, res) => {
+  res.json({
+    message: "Welcome Admin",
+    user: req.user
+  });
+};
 
 // Get all vendors
 export const getVendors = (req, res) => {
   res.json({ message: "List of all vendors will come here" });
 };
 
-
+// Get all delivery agents
+export const getDeliveryPartners = (req, res) => {
+  res.json({ message: "List of all delivery partners will come here" });
+};
