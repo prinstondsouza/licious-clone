c6b8d190e9d03f6691c59b282313395e0102c370
Prinston Dsouza <prinstondsouza42@gmail.com>
2026-01-06 13:01:49 +0530

    new commit

--- server/models/paymentModel.js
+++ server/models/paymentModel.js
@@ -1,42 +1,42 @@
-import mongoose from "mongoose";
+// import mongoose from "mongoose";
 
-const paymentSchema = new mongoose.Schema(
-  {
-    razorpayOrderId: {
-      type: String,
-      required: true,
-    },
-    razorpayPaymentId: {
-      type: String,
-    },
-    razorpaySignature: {
-      type: String,
-    },
-    amount: {
-      type: Number,
-      required: true,
-    },
-    currency: {
-      type: String,
-      default: "INR",
-    },
-    status: {
-      type: String,
-      enum: ["created", "paid", "failed"],
-      default: "created",
-    },
-    user: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-      required: true,
-    },
-    order: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Order",
-      required: true,
-    },
-  },
-  { timestamps: true }
-);
+// const paymentSchema = new mongoose.Schema(
+//   {
+//     razorpayOrderId: {
+//       type: String,
+//       required: true,
+//     },
+//     razorpayPaymentId: {
+//       type: String,
+//     },
+//     razorpaySignature: {
+//       type: String,
+//     },
+//     amount: {
+//       type: Number,
+//       required: true,
+//     },
+//     currency: {
+//       type: String,
+//       default: "INR",
+//     },
+//     status: {
+//       type: String,
+//       enum: ["created", "paid", "failed"],
+//       default: "created",
+//     },
+//     user: {
+//       type: mongoose.Schema.Types.ObjectId,
+//       ref: "User",
+//       required: true,
+//     },
+//     order: {
+//       type: mongoose.Schema.Types.ObjectId,
+//       ref: "Order",
+//       required: true,
+//     },
+//   },
+//   { timestamps: true }
+// );
 
-export default mongoose.model("Payment", paymentSchema);
+// export default mongoose.model("Payment", paymentSchema);
