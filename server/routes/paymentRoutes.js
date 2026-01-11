6f3d5df802ed196b5bec8bad4dc920974b0c815b
Prinston Dsouza <prinstondsouza42@gmail.com>
2026-01-11 17:00:35 +0530

    commit

--- server/routes/paymentRoutes.js
+++ server/routes/paymentRoutes.js
@@ -5,9 +5,9 @@ import { protect } from "../middlewares/authMiddleware.js";
 //   verifyPayment,
 // } from "../controllers/paymentController.js";
 
-// const router = express.Router();
+const router = express.Router();
 
-// router.post("/create-order", protect, createPaymentOrder);
+router.post("/create-order", protect, createPaymentOrder);
 
 // router.post("/verify", protect, verifyPayment);
 
