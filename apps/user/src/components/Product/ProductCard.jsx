3fcc3c042d40ffbd416187beefd22f1506f6b57e
noelquadras <noelquadras22@gmail.com>
2026-01-27 22:18:59 +0530

    feat(ProductCard): Implement image slider animation

--- apps/user/src/components/Product/ProductCard.jsx
+++ apps/user/src/components/Product/ProductCard.jsx
@@ -2,30 +2,52 @@ import { Link } from "react-router-dom";
 import styles from "./ProductCard.module.css";
 import QuantityButton from "./QuantityButton";
 import { useCart } from "../../context/CartContext";
+import { useRef, useState } from "react";
 
 const ProductCard = ({ product, quantity }) => {
   const { addToCart, removeFromCart } = useCart();
+  const images = product.images || [];
+
+  const [activeIndex, setActiveIndex] = useState(0);
+  const intervalRef = useRef(null);
+
+  const startSlide = () => {
+    if (images.length <= 1) return;
+
+    intervalRef.current = setInterval(() => {
+      setActiveIndex((prev) => (prev + 1) % images.length);
+    }, 1200);
+  };
+
+  const stopSlide = () => {
+    clearInterval(intervalRef.current);
+    intervalRef.current = null;
+    setActiveIndex(0);
+  };
 
   return (
     <div className={styles.card}>
-      <div className={styles.imageWrapper}>
-        <Link
-          to={`/product/${product._id}`}
-          style={{ textDecoration: "none", color: "inherit" }}
-        >
-          {product.images?.[0] && (
-            <img
-              src={product.images[0]}
-              alt={product.name}
-              className={styles.image}
-            />
-          )}
+      <div
+        className={styles.imageWrapper}
+        onMouseEnter={startSlide}
+        onMouseLeave={stopSlide}
+      >
+        <Link to={`/product/${product._id}`}>
+          <div
+            className={styles.slider}
+            style={{ transform: `translateX(-${activeIndex * 100}%)` }}
+          >
+            {images.map((img, idx) => (
+              <img key={idx} src={img} className={styles.image} />
+            ))}
+          </div>
         </Link>
+
         <div className={styles.quantityBtn}>
           <QuantityButton
             qty={quantity}
-            onAdd={(qty) => addToCart(product._id)}
-            onRemove={(qty) => removeFromCart(product._id)}
+            onAdd={() => addToCart(product._id)}
+            onRemove={() => removeFromCart(product._id)}
           />
         </div>
       </div>
